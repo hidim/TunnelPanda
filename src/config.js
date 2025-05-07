694e68c6b64f11d76df2564e5362aac5edd3721e
Ali Hıdımoğlu <a.hidim@gmail.com>
2025-05-07 13:59:14 +0300

    feat: Add default values for port and authentication user/pass in config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
+  port: process.env.PORT || 16014,
   ollama: {
     url:   process.env.OLLAMA_API_URL || 'http://localhost:11434',
     apiKey: process.env.OLLAMA_API_KEY || ''
   },
   auth: {
+    user: process.env.BASIC_AUTH_USER || '',
+    pass: process.env.BASIC_AUTH_PASS || '',
     appToken: process.env.APP_TOKEN || ''
   }
 };
